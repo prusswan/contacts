ac2a21c6805b613523633b754ac724e18d4ef920
prusswan <prusswan@gmail.com>
2013-01-08 12:26:33 +0800

    refactoring of factory usage

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe Phone do
   it "has a valid factory" do
-    FactoryGirl.create(:phone).should be_valid
+    create(:phone).should be_valid
   end
 
   it "does not allow duplicate phone numbers per contact" do
     contact = FactoryGirl.create(:contact)
-    FactoryGirl.create(:phone, contact: contact, phone_type: "home", phone: "785-555-1234")
-    FactoryGirl.build(:phone, contact: contact, phone_type: "mobile", phone: "785-555-1234").should_not be_valid
+    create(:home_phone,
+      contact: contact,
+      phone: "785-555-1234")
+    build(:work_phone,
+      contact: contact,
+      phone: "785-555-1234").should_not be_valid
   end
 
   it "allows two contacts to share a phone number" do
-    FactoryGirl.create(:phone, phone_type: "home", phone: "785-555-1234")
-    FactoryGirl.build(:phone, phone_type: "home", phone: "785-555-1234").should be_valid
+    create(:home_phone,
+      phone: "785-555-1234")
+    build(:home_phone,
+      phone: "785-555-1234").should be_valid
   end
 end
